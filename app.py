74948e22b66fbb2f705fd4adaefa295c01cbfdd6
timgrahame <83875027+timgrahame@users.noreply.github.com>
2021-10-08 11:59:24 +0100

    update

--- app.py
+++ app.py
@@ -903,6 +903,8 @@ def create_app():
         }), error.status_code
 
     return app
-
-    if __name__ == '__main__':
-        app.run()
+    
+app = create_app()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
